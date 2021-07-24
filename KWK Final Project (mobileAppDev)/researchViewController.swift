6fd080ca6498a8e30710438b6f6c93ab7af3f974
Emily Martin <martin.e@tx.rr.com>
2021-07-23 19:28:39 -0500

    final changes

--- KWK Final Project (mobileAppDev)/researchViewController.swift
+++ KWK Final Project (mobileAppDev)/researchViewController.swift
@@ -8,9 +8,45 @@
 import UIKit
 
 class researchViewController: UIViewController {
-
+    
+    @IBOutlet weak var fact1: UITextView!
+    
+    @IBOutlet weak var fact2: UITextView!
+    
+    @IBOutlet weak var fact3: UITextView!
+    
+    @IBOutlet weak var fact4: UITextView!
+    
+    @IBOutlet weak var fact5: UITextView!
+    
+    @IBAction func fact1Button(_ sender: UIButton) {
+        fact1.isHidden = false
+    }
+    
+    @IBAction func fact2Button(_ sender: UIButton) {
+        fact2.isHidden = false
+    }
+    
+    @IBAction func fact3Button(_ sender: UIButton) {
+        fact3.isHidden = false
+    }
+    
+    @IBAction func fact4Button(_ sender: UIButton) {
+        fact4.isHidden = false
+    }
+    
+    @IBAction func fact5Button(_ sender: UIButton) {
+        fact5.isHidden = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fact1.isHidden = true
+        fact2.isHidden = true
+        fact3.isHidden = true
+        fact4.isHidden = true
+        fact5.isHidden = true
 
         // Do any additional setup after loading the view.
     }
